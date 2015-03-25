381589f25d9a8fda8540e6c3969ded835d94f345
kruczjak <kruczjak@gmail.com>
2015-03-25 16:38:26 +0100

    2.3 working

--- cw02/zad3/main.c
+++ cw02/zad3/main.c
@@ -16,6 +16,26 @@
 #define KNRM  "\x1B[0m"
 #define KRED  "\x1B[31m"
 #define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+
+#define LOCKED_INFO "It seems that another process has locked this byte\n\n"
+int fcntl_struct(int,int,int,off_t,int,off_t);
+int getLock(int, long int);
+
+void removeLock(int handle) {
+	long int byt;
+	printf("Set byte number: ");
+	scanf("%ld", &byt);
+	if((fcntl_struct(handle, F_SETLK, F_UNLCK, byt, SEEK_SET, 1)) == -1){
+		if(errno == EACCES || errno == EAGAIN)
+			printf("%s%s", KRED, LOCKED_INFO);
+		else {
+			printf(KRED"\nError in function w_lock() -> fcntl()\nMessage: \"%s\", errno: %d\n\n", strerror(errno), errno);
+		}
+		return;
+	}
+	printf(KGRN"Should be unlocked");
+}
 int fcntl_struct(int file_des, int cmd, int type, off_t offset, int whence, off_t len){
 	struct flock lock;
 
@@ -25,6 +45,19 @@ int fcntl_struct(int file_des, int cmd, int type, off_t offset, int whence, off_
 	lock.l_whence = whence;
 
 	return fcntl(file_des, cmd, &lock);
+}
+int getLock(int handle, long int byt) {
+  struct flock lock;
+	lock.l_len = 0;
+	lock.l_start = byt;
+	lock.l_type = F_WRLCK;
+	lock.l_whence = SEEK_SET;
+
+	if((fcntl(handle, F_GETLK, &lock)) == -1){
+		printf(KRED"\nError in function is_locked() -> fcntl()\nMessage: \"%s\", errno: %d\n\n", strerror(errno), errno);
+		return -1;
+	}
+	return lock.l_type; // F_WRCLK / F_RDLCK
 }
 void listLocks(int handle) {
   int end_of_loop = 0;
@@ -37,7 +70,7 @@ void listLocks(int handle) {
 
 	while(!end_of_loop){
 		if((fcntl(handle, F_GETLK, &lock)) == -1){
-			printf("\nError in function list_locks() -> fcntl()\nMessage: \"%s\", errno: %d\n\n", strerror(errno), errno);
+			printf(KRED"\nError in function list_locks() -> fcntl()\nMessage: \"%s\", errno: %d\n\n", strerror(errno), errno);
       return;
 		}
 		if(lock.l_type != F_UNLCK){
@@ -48,7 +81,7 @@ void listLocks(int handle) {
 			else if(lock.l_type == F_WRLCK)
 				lock_type = "";
 
-			printf("PID %d has %slocked byte %d\n", (int) lock.l_pid, lock_type, (int) lock.l_start);
+			printf(KYEL"PID %d has %slocked byte %d\n", (int) lock.l_pid, lock_type, (int) lock.l_start);
 			lock.l_len = 0;
 			lock.l_start++;
 			lock.l_whence = SEEK_SET;
@@ -56,6 +89,7 @@ void listLocks(int handle) {
 		else
 			end_of_loop = 1;
 	}
+	printf("\n");
 }
 void readByte(int handle) {
   char buf;
@@ -66,11 +100,29 @@ void readByte(int handle) {
   read(handle, &buf, 1);
   printf("%sByte %ld = %c\n\n", KGRN, byt, buf);
 }
+void readLock(int handle) {
+  long int byt;
+  printf("Set byte number: ");
+  scanf("%ld", &byt);
+  if((fcntl_struct(handle, F_SETLK, F_RDLCK, byt, SEEK_SET, 1)) == -1){
+		if(errno == EACCES || errno == EAGAIN)
+			printf(KRED LOCKED_INFO);
+		else {
+			printf("\nError in function w_lock() -> fcntl()\nMessage: \"%s\", errno: %d\n\n", strerror(errno), errno);
+		}
+    return;
+  }
+  printf("%sShould be read locked", KGRN);
+}
 void writeByte(int handle) {
   char buf;
   long int byt;
   printf("Set byte number: ");
   scanf("%ld", &byt);
+  if((getLock(handle, byt)) != F_UNLCK){
+		printf(KRED LOCKED_INFO);
+		return;
+	}
   lseek(handle, byt, SEEK_SET);
   printf("Char to write: ");
   scanf(" %c", &buf);
@@ -83,13 +135,13 @@ void writeLock(int handle) {
   scanf("%ld", &byt);
   if((fcntl_struct(handle, F_SETLK, F_WRLCK, byt, SEEK_SET, 1)) == -1){
 		if(errno == EACCES || errno == EAGAIN)
-			printf("It seems that another process has locked this byte\n"
-					"type 'list_locks' to get more informations\n");
+			printf(KRED LOCKED_INFO);
 		else {
 			printf("\nError in function w_lock() -> fcntl()\nMessage: \"%s\", errno: %d\n\n", strerror(errno), errno);
-			return;
 		}
+    return;
   }
+  printf("%sShould be write locked", KGRN);
 }
 void clearScreen()
 {
@@ -121,6 +173,7 @@ int main(int argc, char **argv) {
     }
     switch (input) {
         case '1':
+					readLock(handle);
           break;
         case '2':
           writeLock(handle);
@@ -128,6 +181,9 @@ int main(int argc, char **argv) {
         case '3':
           listLocks(handle);
           break;
+				case '4':
+					removeLock(handle);
+					break;
         case '5':
           readByte(handle);
           break;
