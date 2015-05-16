413ac5fafe3a63203bdad81534c3d148349d46ae
Paulina Lach <smig007@wp.pl>
2015-05-16 22:27:53 +0200

    vol1.02

--- cw07/zad1/main.c
+++ cw07/zad1/main.c
@@ -6,51 +6,19 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <time.h>
+#include <unistd.h>
 
 #define N 100
 
-int * TAB;
 int seg_id;
 
-int main(int argc, char ** argv) {
+struct Segment {
+  int TAB[N];
+  int tab_ptr;
+};
 
-  if (argc != 2) {
-
-  }
-
-
-  key_t key;
-  int shmflg = 0;
-
-  int seg_size = sizeof(TAB);
-
-  //allocate shared mem seg,
-  if ((seg_id = shmget(IPC_PRIVATE, seg_size, IPC_CREAT | S_IRUSR | S_IWUSR)) < 0) {
-
-  }
-
-  atexit(clean);
-
-  //attach sh mem seg
-  if (*(TAB = (int *) shmat(seg_id, 0, 0)) < 0) {
-
-  }
-
-
-
-  if (strcmp(argv[1], "p") == 0) {
-    producent();
-  }
-  else if (strcmp(argv[1], "c") == 0) {
-    customer();
-  }
-  else {
-    //error
-  }
-
-
-  return 0;
-}
+void * segment;
 
 bool checkPrime(int x) {
   if (x == 0 || x == 1) {
@@ -67,7 +35,99 @@ bool checkPrime(int x) {
   return true;
 }
 
+
+
 void clean() {
-  shmdt(TAB);
+  shmdt(segment);
   shmctl(seg_id, IPC_RMID, 0);
+}
+
+
+void producent() {
+
+  printf("1");
+  while(1) {
+    int number = rand();
+    struct Segment * curr = (struct Segment *) segment;
+    //printf("%d", curr->TAB[curr->tab_ptr]);
+    if (curr->TAB[curr->tab_ptr] == -1) {
+      curr->TAB[curr->tab_ptr++] = number;
+      if (curr->tab_ptr == N) {
+        curr->tab_ptr = 0;
+      }
+      int counter = 0;
+      for (int i = 0; i < N; ++i) {
+        if (curr->TAB[i] != -1) {
+          counter += 1;
+        }
+      }
+      printf("(%d %ld)   Dodalem liczbe: %d. Liczba zadan oczekujacych: %d. \n", getpid(), time(NULL), number, counter);
+    }
+    else {
+      break;
+    }
+
+  }
+
+}
+
+
+void customer() {}
+
+
+int main(int argc, char ** argv) {
+
+  if (argc != 2) {
+
+  }
+
+  srand(time(NULL));
+
+  key_t key;
+  int shmflg = 0;
+
+  int seg_size = sizeof(struct Segment);
+
+  //allocate shared mem seg,
+  if ((seg_id = shmget(IPC_PRIVATE, seg_size, IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR)) >= 0) {
+    if (*((int *)(segment = shmat(seg_id, 0, 0))) < 0) {
+      //error
+    }
+    // struct Segment to_realloc;
+    // to_realloc.tab_ptr = 0;
+    //
+    // memcpy(&to_realloc, segment, sizeof(to_realloc));
+    for (int i = 0; i < N; i += 1) {
+      ((struct Segment * ) segment)->TAB[i] = -1;
+    }
+  }
+  else if ((seg_id = shmget(IPC_PRIVATE, seg_size, S_IRUSR | S_IWUSR)) < 0) {
+    //error
+  }
+  else if (*((int *)(segment = shmat(seg_id, 0, 0))) < 0) {
+
+  }
+
+
+  atexit(clean);
+
+  //attach sh mem seg
+  //if (*((int *)(segment = shmat(seg_id, 0, 0))) < 0) {
+  //
+  //}
+
+
+
+  if (strcmp(argv[1], "p") == 0) {
+    producent();
+  }
+  else if (strcmp(argv[1], "c") == 0) {
+    customer();
+  }
+  else {
+    printf("Wchodzi!");
+  }
+
+
+  return 0;
 }
