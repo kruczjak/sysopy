2f1555dc599042ca8463d8b3945a8568337227b8
kruczjak <kruczjak@gmail.com>
2015-05-17 18:36:07 +0200

    Semaphors 7.1

--- cw07/zad1/main.c
+++ cw07/zad1/main.c
@@ -9,9 +9,14 @@
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
-
+#include <errno.h>
+#include <sys/time.h>
+#include <signal.h>
 
 #define N 100
+#define ERROR { int error_code = errno; \
+				printf("FATAL (line %d): %s\n", __LINE__, strerror(error_code)); \
+				exit(error_code);}
 
 int seg_id;
 int semid;
@@ -26,7 +31,7 @@ union semun {
   struct semid_ds * buf;
   unsigned short int * array;
   struct seminfo * __buf;
-}
+};
 
 void * segment;
 
@@ -45,7 +50,25 @@ bool checkPrime(int x) {
   return true;
 }
 
+void up(int n) {
+  struct sembuf semOp;
+  semOp.sem_num = n;
+  semOp.sem_op = 1;
+  semOp.sem_flg = 0;
+  if (semop(semid, &semOp, 1) < 0) {
+		ERROR;
+	}
+}
 
+void down(int n) {
+  struct sembuf semOp;
+  semOp.sem_num = n;
+  semOp.sem_op = -1;
+  semOp.sem_flg = 0;
+  if (semop(semid, &semOp, 1) < 0) {
+		ERROR;
+	}
+}
 
 void clean() {
   shmdt(segment);
@@ -54,127 +77,144 @@ void clean() {
   semctl(semid, 1, IPC_RMID, ignored_argument);
 }
 
+void handler() {
+	exit(0);
+}
 
 void producent() {
-
-  printf("1");
+	struct Segment * curr = (struct Segment *) segment;
   while(1) {
     int number = rand();
-    struct Segment * curr = (struct Segment *) segment;
-    //printf("%d", curr->TAB[curr->tab_ptr]);
-    if (curr->TAB[curr->tab_ptr] == -1) {
-      curr->TAB[curr->tab_ptr++] = number;
+		down(0);
+    down(2);
+
+		while (curr->TAB[curr->tab_ptr] != -1) {
+      curr->tab_ptr += 1;
       if (curr->tab_ptr == N) {
         curr->tab_ptr = 0;
       }
-      int counter = 0;
-      for (int i = 0; i < N; ++i) {
-        if (curr->TAB[i] != -1) {
-          counter += 1;
-        }
-      }
-      printf("(%d %ld)   Dodalem liczbe: %d. Liczba zadan oczekujacych: %d. \n", getpid(), time(NULL), number, counter);
-    }
-    else {
-      break;
     }
 
+    int counter = 0;
+    for (int i = 0; i < N; ++i) {
+      if (curr->TAB[i] != -1) {
+        counter += 1;
+      }
+    }
+		struct timeval  tv;
+		gettimeofday(&tv, NULL);
+		double time_in_mill =
+       (tv.tv_sec) * 1000 + (tv.tv_usec) / 1000 ; // convert tv_sec & tv_usec to millisecond
+    printf("(%d %.0lf)   Dodalem liczbe: %d. Liczba zadan oczekujacych: %d. \n", getpid(), time_in_mill, number, counter);
+
+		up(2);
+		up(1);
   }
 
 }
 
 
 void customer() {
+  struct Segment * curr = (struct Segment *) segment;
 
   while(1) {
-    //int number = rand();
-    struct Segment * curr = (struct Segment *) segment;
+		down(1);
+    down(2);
     while (curr->TAB[curr->tab_ptr] == -1) {
-      tab_ptr += 1;
+      curr->tab_ptr += 1;
       if (curr->tab_ptr == N) {
         curr->tab_ptr = 0;
+      }
     }
-    int counter = 0;
-    for (int i = 0; i < N; ++i) {
-      if (curr->TAB[i] != -1) {
-        counter += 1;
-    }
+
     int number = curr-> TAB[curr->tab_ptr];
     curr-> TAB[curr->tab_ptr] = -1;
-    //printf("%d", curr->TAB[curr->tab_ptr]);
-    // if (curr->TAB[curr->tab_ptr] == -1) {
-    //   curr->TAB[curr->tab_ptr++] = number;
-    //   if (curr->tab_ptr == N) {
-    //     curr->tab_ptr = 0;
-    //   }
+		int counter = 0;
+		for (int i = 0; i < N; ++i) {
+			if (curr->TAB[i] != -1) {
+				counter += 1;
+			}
+		}
+    up(2);
+		up(0);
 
-    //  }
     char * isFirst = "pierwsza";
     if (!checkPrime(number)) {
       isFirst = "zlozona";
     }
-      printf("(%d %ld)   Sprawdzilem liczbe: %d - %s. Pozostalo zadan oczekujacych: %d. \n", getpid(), time(NULL), number, ifFirst, counter);
+		struct timeval  tv;
+		gettimeofday(&tv, NULL);
+		double time_in_mill =
+			(tv.tv_sec) * 1000 + (tv.tv_usec) / 1000 ; // convert tv_sec & tv_usec to millisecond
+    printf("(%d %.0lf)   Sprawdzilem liczbe: %d - %s. Pozostalo zadan oczekujacych: %d. \n", getpid(), time_in_mill, number, isFirst, counter);
     }
-    else {
-      break;
-    }
-
   }
 
-
-}
 
 
 int main(int argc, char ** argv) {
 
   if (argc != 2) {
-
+    printf("Argument mismatch!: main.run <c|p>\n");
+    exit(1);
   }
 
+	if (signal(SIGINT, handler) == SIG_ERR) {
+			ERROR;
+	}
   srand(time(NULL));
 
-  key_t key;
-  int shmflg = 0;
+	key_t key;
+  if ( (key = ftok(argv[0], 's')) < 0 ) {
+    ERROR;
+  }
+  // sem 0: prod == emptyCount = MAX
+	// sem 1: cons == fillCount = 0
+	// sem 2: tab == mutex = 1
+  if ((semid = semget(key, 3, IPC_CREAT | IPC_EXCL | 0666)) >= 0) {
+		union semun arg_empty;
+		union semun arg_full;
+		union semun arg_critic;
+		arg_empty.val = N;
+		arg_full.val = 0;
+		arg_critic.val = 1;
+		if (semctl(semid, 0, SETVAL, arg_empty) < 0) {
+				ERROR;
+		}
+		if (semctl(semid, 1, SETVAL, arg_full) < 0) {
+				ERROR;
+		}
+		if (semctl(semid, 2, SETVAL, arg_critic) < 0) {
+				ERROR;
+		}
+	} else if ((semid = semget(key, 3, 0)) < 0) {
+		ERROR;
+	}
 
-  semget(IPC_PRIVATE, 1, 0);
-  union semun argument;
-  unsigned short values[1];
-  values[0] = 1;
-  argument.array = values;
-  semctl(semid, 0, SETALL, argument);
 
   int seg_size = sizeof(struct Segment);
+  atexit(clean);
 
   //allocate shared mem seg,
-  if ((seg_id = shmget(IPC_PRIVATE, seg_size, IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR)) >= 0) {
-    if (*((int *)(segment = shmat(seg_id, 0, 0))) < 0) {
-      //error
+	if ( (key = ftok(argv[0], 'm')) < 0 ) {
+    ERROR;
+  }
+  if ((seg_id = shmget(key, seg_size, IPC_CREAT | IPC_EXCL | 0666)) >= 0) {
+    //success
+		if (*((int *)(segment = shmat(seg_id, NULL, 0))) < 0) {
+      ERROR;
     }
-    // struct Segment to_realloc;
-    // to_realloc.tab_ptr = 0;
-    //
-    // memcpy(&to_realloc, segment, sizeof(to_realloc));
     ((struct Segment * ) segment)->tab_ptr = 0;
     for (int i = 0; i < N; i += 1) {
       ((struct Segment * ) segment)->TAB[i] = -1;
     }
   }
-  else if ((seg_id = shmget(IPC_PRIVATE, seg_size, S_IRUSR | S_IWUSR)) < 0) {
-    //error
+  else if ((seg_id = shmget(key, seg_size, 0)) < 0) {
+    ERROR;
   }
-  else if (*((int *)(segment = shmat(seg_id, 0, 0))) < 0) {
-
+  else if (*((int *)(segment = shmat(seg_id, NULL, 0))) < 0) {
+    ERROR;
   }
-
-
-  atexit(clean);
-
-  //attach sh mem seg
-  //if (*((int *)(segment = shmat(seg_id, 0, 0))) < 0) {
-  //
-  //}
-
-
 
   if (strcmp(argv[1], "p") == 0) {
     producent();
@@ -183,7 +223,8 @@ int main(int argc, char ** argv) {
     customer();
   }
   else {
-    printf("Wchodzi!");
+    printf("Argument mismatch!: main.run <c|p>\n");
+    exit(1);
   }
 
 
