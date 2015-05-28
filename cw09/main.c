a5b5a6fb17e6bd595c1077f5124e654dd2ed6063
kruczjak <kruczjak@gmail.com>
2015-05-28 22:54:54 +0200

    9.0 Fixes

--- cw09/main.c
+++ cw09/main.c
@@ -3,82 +3,73 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
 
-pthread_t tid[5];
+#define ERROR {printf("FATAL (line %d): %s\n", __LINE__, strerror(errno)); \
+				exit(errno);}
+
+pthread_t threads[5];
 int counter;
-pthread_mutex_t lock;
-pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t mutex;
+pthread_cond_t condition = PTHREAD_COND_INITIALIZER;
 
-pthread_cond_t fork_cond[5] = {PTHREAD_COND_INITIALIZER};
-int forks[5] = {0};                             //0 means free fork
+pthread_cond_t forkCondition[5] = {PTHREAD_COND_INITIALIZER};
+int forks[5] = {0};
 
-void* doPhilosophy(void *arg)
+void* dinner(void *arg)
 {
     int i;
     int id = (int)arg;
-    pthread_mutex_lock(&lock);
+    pthread_mutex_lock(&mutex);
     if(counter == 4) {                          //only 4 philosophers in monitor
-        pthread_cond_wait(&cond, &lock);
+        pthread_cond_wait(&condition, &mutex);
     }
     counter++;
-    pthread_mutex_unlock(&lock);
+    pthread_mutex_unlock(&mutex);
 
-    pthread_mutex_lock(&lock);                  //taking forks
+    pthread_mutex_lock(&mutex);                  //taking forks
     if(forks[id] == 1) {
-        pthread_cond_wait(&fork_cond[id], &lock);
+        pthread_cond_wait(&forkCondition[id], &mutex);
     }
     forks[id] = 1;
     if(forks[(id+1)%5] == 1) {
-        pthread_cond_wait(&fork_cond[(id+1)%5], &lock);
+        pthread_cond_wait(&forkCondition[(id+1)%5], &mutex);
     }
     forks[(id+1)%5] = 1;
-    pthread_mutex_unlock(&lock);
+    pthread_mutex_unlock(&mutex);
 
-    printf("Consumption %d started\n", id);     //consumption
-    for(i=0; i<(0xFFFFFFFF);i++);
-    printf("Consumption %d finished\n", id);
+    printf("Eating %d start\n", id);     //consumption
+    for(i=0; i<(0xFFFFFF);i++);
+    printf("Eating %d ended\n", id);
 
-    pthread_mutex_lock(&lock);                  //returning forks
+    pthread_mutex_lock(&mutex);                  //returning forks
     forks[id] = 0;
     forks[(id+1)%5] = 0;
-    pthread_cond_signal(&fork_cond[id]);
-    pthread_cond_signal(&fork_cond[(id+1)%5]);
-    pthread_mutex_unlock(&lock);
+    pthread_cond_signal(&forkCondition[id]);
+    pthread_cond_signal(&forkCondition[(id+1)%5]);
 
-    pthread_mutex_lock(&lock);                  //leaving monitor
     counter--;
-    pthread_cond_signal(&cond);
-    pthread_mutex_unlock(&lock);
+    pthread_cond_signal(&condition);
+    pthread_mutex_unlock(&mutex);
 
     return NULL;
 }
 
 int main(void)
 {
-    int i = 0;
-    int err;
     counter = 0;
 
-    if (pthread_mutex_init(&lock, NULL) != 0)
-    {
-        printf("\n mutex init failed\n");
-        return 1;
-    }
+    if (pthread_mutex_init(&mutex, NULL) != 0) ERROR;
 
-    while(i < 5)
-    {
-        err = pthread_create(&(tid[i]), NULL, &doPhilosophy, (void*) i);
-        if (err != 0)
-            printf("\ncan't create thread :[%s]", strerror(err));
-        i++;
-    }
+    for (int i = 0; i < 5; i++)
+        if (pthread_create(&(threads[i]), NULL, &dinner, (void*) i) !=0) ERROR;
 
-    pthread_join(tid[0], NULL);
-    pthread_join(tid[1], NULL);
-    pthread_join(tid[2], NULL);
-    pthread_join(tid[3], NULL);
-    pthread_join(tid[4], NULL);
-    pthread_mutex_destroy(&lock);
+    pthread_join(threads[0], NULL);
+    pthread_join(threads[1], NULL);
+    pthread_join(threads[2], NULL);
+    pthread_join(threads[3], NULL);
+    pthread_join(threads[4], NULL);
+    pthread_mutex_destroy(&mutex);
 
     return 0;
 }
