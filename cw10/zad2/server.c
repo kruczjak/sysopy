838475fb4c6f8153579136250bb252ee6e51708d
kruczjak <kruczjak@gmail.com>
2015-06-12 04:24:26 +0200

    10.2 Server

--- cw10/zad2/server.c
+++ cw10/zad2/server.c
@@ -19,98 +19,80 @@ int sockets[10];
 int socket_net, socket_unix, number = 0;
 char *file;
 
-typedef struct msg
-{
+typedef struct msg {
     char from[32];
     char content[480];
 } msg;
 
-int exception (int ret, char* msg)
-{
-    if(ret == -1)
-    {
-        printf("%s\n", msg);
-        perror("");
-        exit(-1);
-    }
-    return ret;
-}
-
-void exit_handler()
-{
+void exit_handler() {
     exit(0);
 }
 
-void exitme()
-{
+void exitme() {
     int i;
-    for(i = 0; i < number; i++)
-    {
+    for(i = 0; i < number; i++) {
         shutdown(sockets[i], SHUT_RDWR);
-        exception(close(sockets[i]), "Close error.");
+        if (close(sockets[i]) < 0) ERROR;
     }
-    exception(close(socket_net), "Close error.");
-    exception(close(socket_unix), "Close error.");
-    exception(unlink(file), "Unlink error.");
+    if (close(socket_net) < 0) ERROR;
+    if (close(socket_unix) < 0) ERROR;
+    if (unlink(file) < 0) ERROR;
 }
 
-void trap_signal(int sig, void (*handler)(void))
-{
+void trap_signal(int sig, void (*handler)(int)) {
     struct sigaction act_usr;
-    exception(sigemptyset(&act_usr.sa_mask), "Couldn't initialize a signal set.");
-    exception(sigaddset(&act_usr.sa_mask, sig), "Couldn't add a signal to the set.");
-    act_usr.sa_handler = (void*)(handler);
+    if (sigemptyset(&act_usr.sa_mask) < 0) ERROR;
+    if (sigaddset(&act_usr.sa_mask, sig) < 0) ERROR;
+    act_usr.sa_handler = handler;
     act_usr.sa_flags = 0;
-    exception(sigaction(sig, &act_usr, NULL), "Sigaction failed.");
+    if (sigaction(sig, &act_usr, NULL) < 0) ERROR;
 }
 
-void init(char *path)
-{
+void init(char *path) {
     file = path;
     trap_signal(SIGINT, &exit_handler);
-    exception(atexit(&exitme), "Couldn't set cleaning function.");
+    if (atexit(&exitme) < 0) ERROR;
 }
 
-void create_unix_socket()
-{
+void create_unix_socket() {
     struct sockaddr_un addr_unix;
 
-    socket_unix = exception(socket(AF_UNIX, SOCK_STREAM, 0), "Cannot create UNIX domain socket.");
+    if ((socket_unix = socket(AF_UNIX, SOCK_STREAM, 0)) < 0) ERROR;
 
     memset(&addr_unix, 0, sizeof(addr_unix));
     addr_unix.sun_family = AF_UNIX;
     strncpy(addr_unix.sun_path, file, sizeof(addr_unix.sun_path) - 1);
     addr_unix.sun_path[sizeof(addr_unix.sun_path) - 1] = '\0';
 
-    exception(bind(socket_unix, (struct sockaddr*)&addr_unix, sizeof(addr_unix)), "Bind UNIX domain socket failed.");
-    exception(listen(socket_unix, 10), "Listen failed.");
+    if (bind(socket_unix, (struct sockaddr*)&addr_unix, sizeof(addr_unix)) < 0) ERROR;
+    if (listen(socket_unix, 10) < 0) ERROR;
 }
 
-void create_net_socket(int port)
-{
+void create_net_socket(int port) {
     struct sockaddr_in addr_net;
 
-    socket_net = exception(socket(PF_INET, SOCK_STREAM, IPPROTO_TCP), "Cannot create TCP socket.");
+    if ((socket_net = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) ERROR;
 
     memset(&addr_net, 0, sizeof(addr_net));
     addr_net.sin_family = AF_INET;
     addr_net.sin_port = htons(port);
     addr_net.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    exception(bind(socket_net, (struct sockaddr*)&addr_net, sizeof(addr_net)), "Bind TCP socket failed.");
-    exception(listen(socket_net, 10), "Listen failed.");
+    if (bind(socket_net, (struct sockaddr*)&addr_net, sizeof(addr_net)) < 0) ERROR;
+    if (listen(socket_net, 10) < 0) ERROR;
 }
 
 
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     fd_set set;
     int i, j, max;
     msg message;
 
-    if(argc != 3)
-        exception(-1, "Two arguments needed: port number (greater than 1000) and a filepath for the UNIX domain socket.\n");
+    if(argc != 3) {
+				printf("Bad arguments: server.run <port> <path>\n");
+				exit(1);
+		}
 
     init(argv[2]);
     create_unix_socket();
@@ -121,8 +103,7 @@ int main(int argc, char *argv[])
     else
         max = socket_unix + 1;
 
-    while(true)
-    {
+    while(true) {
         FD_ZERO(&set);
         FD_SET(socket_unix, &set);
         FD_SET(socket_net, &set);
@@ -130,14 +111,12 @@ int main(int argc, char *argv[])
             FD_SET(sockets[i], &set);
 
 
-        exception(select(max, &set, NULL, NULL, NULL), "Select error.");
+        if (select(max, &set, NULL, NULL, NULL) < 0) ERROR;
 
 
-        if(FD_ISSET(socket_unix, &set))
-        {
+        if(FD_ISSET(socket_unix, &set)) {
             sockets[number] = accept(socket_unix, NULL, NULL);
-            if(0 > sockets[number])
-            {
+            if(0 > sockets[number]) {
               perror("error accept failed");
               close(sockets[number] );
               exit(EXIT_FAILURE);
@@ -148,11 +127,9 @@ int main(int argc, char *argv[])
             number++;
         }
 
-        if(FD_ISSET(socket_net, &set))
-        {
+        if(FD_ISSET(socket_net, &set)) {
             sockets[number] = accept(socket_net, NULL, NULL);
-            if(0 > sockets[number])
-            {
+            if(0 > sockets[number]) {
               perror("error accept failed");
               close(sockets[number]);
               exit(EXIT_FAILURE);
@@ -165,15 +142,14 @@ int main(int argc, char *argv[])
 
 
         for(i = 0; i < number; i++)
-            if(FD_ISSET(sockets[i], &set))
-            {
+            if(FD_ISSET(sockets[i], &set)) {
                 memset(&message, 0, sizeof(msg));
                 if(read(sockets[i], &message, sizeof(msg)) == 0)
                     sockets[i] = sockets[--number];
                 else
                     for(j = 0; j< number; j++)
                         if(j != i)
-                            exception(write(sockets[j], &message, sizeof(msg)), "Write error.");
+                            if (write(sockets[j], &message, sizeof(msg)), "Write error.");
             }
     }
 
